22b80e3d9d2833dc10880ee083a7bcf8138f5344
MMichotte <martin.michotte@gmail.com>
2021-08-31 16:58:27 +0200

    basic frontend setup

---
+++ frontend/src/app/features/login/pages/login.component.ts
@@ -0,0 +1,51 @@
+// import { AuthService } from '@core/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  // constructor (private auth: AuthService, private router: Router) { }
+  constructor (private router: Router) { }
+
+    loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    })
+
+    errorMsg: string;
+    displayError: boolean = false;
+    displaySpinner: boolean = false;
+
+    ngOnInit (): void {
+      // this.auth.logout();
+    }
+
+    onSubmit (): void {
+      /*
+      this.displaySpinner = true;
+      this.auth.loginUser(this.loginForm.value).subscribe(
+        (res: any) => {
+          this.auth.setLogin(res.token);
+          this.router.navigate(['/']);
+        },
+        error => {
+          if (error.status === 401) {
+            this.errorMsg = 'Incorrect email or password';
+          }
+          this.displayError = true;
+          this.displaySpinner = false;
+          console.log(error);
+        }
+      );*/
+    }
+
+    hideError (): void {
+      this.displayError = false;
+    }
+}
